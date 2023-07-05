3fc2c59dca14f4de73e4ccf74e871694e6000dea
Nisha Bista <Nishabista053@gmail.com>
2023-07-05 14:44:09 -0500

    case_exercise

--- case_exercises.sql
+++ case_exercises.sql
@@ -1,31 +1,14 @@
 USE employees;
 
 -- 1.Write a query that returns all employees, their department number, their start date, their end date, and a new column 'is_current_employee' that is a 1 if the employee is still with the company and 0 if not. DO NOT WORRY ABOUT DUPLICATE EMPLOYEES.
- SELECT 
-  e.emp_no,
-  e.first_name,
-  e.last_name,
-  d.from_date AS start_date,
-  d.to_date AS end_date,
-  CASE
-    WHEN d.to_date IS NULL THEN 1
-    ELSE 0
-  END AS is_current_employee
-FROM employees e
-INNER JOIN dept_emp AS d ON e.emp_no = d.emp_no;
-
-
--- FROM employees AS e
--- INNER JOIN dept_emp AS d ON e.emp_no = d.emp_no;  
- # CASE
-    #WHEN d.end_date IS NULL THEN 1
-    #ELSE 0
-  #END AS is_current_employee
-#FROM employees e
-#INNER JOIN dept_emp d ON e.emp_no = d.emp_no;
-
+ SELECT CONCAT(first_name,'',last_name),from_date, to_date, dept_no,
+  IF (to_date > CURDATE(), TRUE, FALSE) AS is_current_employee
+  FROM employees
+  JOIN dept_emp ON employees.emp_no = dept_emp.emp_no;
+  
+  
 -- 2.Write a query that returns all employee names (previous and current), and a new column 'alpha_group' that returns 'A-H', 'I-Q', or 'R-Z' depending on the first letter of their last name.
-SELECT emp_no, first_name, last_name,
+SELECT  last_name,
 CASE 
     WHEN SUBSTRING(last_name, 1) BETWEEN 'A' AND 'H' THEN 'A-H'
     WHEN SUBSTRING(last_name, 1) BETWEEN 'I' AND 'Q' THEN 'I-Q'
@@ -36,27 +19,39 @@ FROM employees;
 
 -- 3. How many employees (current or previous) were born in each decade?
 SELECT 
-  YEAR(birth_date) - YEAR(birth_date) % 10 AS birth_decade,
-  COUNT(*) AS num_employees
-FROM employees
-GROUP BY birth_decade
-ORDER BY birth_decade;
+	CASE
+		WHEN substr(birth_date, 1, 4) BETWEEN 1950 AND 1959 THEN '1950s'
+       --  WHEN substr(birth_date, 1, 4) BETWEEN 1960 AND 1969 THEN '1960s'
+       ELSE '1960s'
+        END AS 'birth_decade', 
+	COUNT(emp_no)
+	FROM employees
+	GROUP BY birth_decade;
 -- 1950 num of employees were 182886
 -- 1960 num of employees were 117138
 
 -- 4.What is the current average salary for each of the following department groups: R&D, Sales & Marketing, Prod & QM, Finance & HR, Customer Service?
-SELECT
-  dept_name,
-  CASE WHEN title = R$D
-  CASE WHEN title = Sales & Marketing
-  CASE WHEN title = Prod & QM
-  CASE WHEN title = Finance  $ HR
-  
-  
- 
-SELECT  titles
-FROM employees;
+-- SELECT R&D, Sales & Marketing, Prod & QM, Finance & HR, Customer Service,
+  SELECT AVG(salary), 
+  CASE
+       WHEN dept_name IN ('research', 'development') THEN 'R&D'
+       WHEN dept_name IN ('sales', 'marketing') THEN 'Sales & Marketing'
+       WHEN dept_name IN ('Production', 'Quality Management') THEN 'Prod & QM'
+       WHEN dept_name IN ('Finance', 'Human Resources') THEN 'Finance & HR'
+       ELSE 'Customer Service'
+   END AS dept_group
+   FROM dept_emp
+   LEFT JOIN salaries ON dept_emp.emp_no = salaries.emp_no AND salaries.to_date > CURDATE()
+   LEFT JOIN departments ON dept_emp.dept_no = departments.dept_no
+GROUP BY dept_group;
 
+-- BONUS
+-- Remove duplicate employees from exercise 1.
+
+SELECT DISTINCT(employees.emp_no), CONCAT(first_name,' ',last_name), dept_no, from_date, to_date, 
+IF(to_date > CURDATE(), TRUE, FALSE) AS is_current_employee
+FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no;
 
 
 
