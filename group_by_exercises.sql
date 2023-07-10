1af53dbb93d87d1012a1009fe89d234bfcaf7083
Nisha Bista <Nishabista053@gmail.com>
2023-07-10 07:48:21 -0500

    group_by_exercises

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -32,8 +32,8 @@ HAVING last_name LIKE '%q%' AND last_name NOT LIKE  '%qu%';
 -- 7.Find all all employees with first names 'Irena', 'Vidya', or 'Maya'. Use COUNT(*) and GROUP BY to find the number of employees for each gender with those names.
 SELECT first_name, gender, COUNT(*) AS emp_count
 FROM employees
-GROUP BY first_name, gender
-HAVING first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY first_name, gender;
 
 
 -- 8.Using your query that generates a username for all of the employees, generate a count employees for each unique username.
@@ -45,16 +45,13 @@ FROM (
 GROUP BY username;
 
 -- 9.From your previous query, are there any duplicate usernames? What is the highest number of times a username shows up? Bonus: How many duplicate usernames are there from your previous query?
-SELECT username, COUNT(*) AS emp_count
-FROM (
-  SELECT CONCAT(LOWER(SUBSTRING(first_name, 1, 1)), LOWER(SUBSTRING(last_name, 1, 4)), '_', SUBSTR(birth_date,6,2), SUBSTR(birth_date,3,2)) AS username
-  FROM employees
-) AS employee_usernames
-GROUP BY username
-HAVING emp_count > 1
-ORDER BY COUNT(*) DESC;
--- most showed username is 3
--- there are 1000 duplicates
+SELECT LOWER(CONCAT(SUBSTR(first_name, 1, 1), SUBSTR(last_name, 1, 4), '_', SUBSTR(birth_date, 6, 2), SUBSTR(birth_date, 3, 2))) AS user_name, COUNT(*) AS number_usernames
+FROM employees
+GROUP BY user_name
+HAVING number_usernames > 1
+ORDER BY number_usernames DESC;
+-- most showed username is 6
+-- there are 13,251 duplicates
 
 -- BONUS MORE PRACTICE WITH AGGREGATE FUNCTION
 
